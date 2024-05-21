b6ed9a11c7440cf2729e384b1d9bdab128c125b7
MariyaPro <mariaprokof@gmail.com>
2024-05-21 17:23:54 +0300

    Initial commit

---
+++ deal_api/src/main/java/com/prokofeva/deal_api/DealApiApplication.java
@@ -0,0 +1,13 @@
+package com.prokofeva.deal_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealApiApplication.class, args);
+    }
+
+}
