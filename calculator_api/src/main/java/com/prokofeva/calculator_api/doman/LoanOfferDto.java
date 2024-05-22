160b1763591b28fd5eab0190a8442c3015b17d86
MariyaPro <mariaprokof@gmail.com>
2024-05-22 11:13:00 +0300

    create LoanOfferService

--- calculator_api/src/main/java/com/prokofeva/calculator_api/doman/LoanOfferDto.java
+++ calculator_api/src/main/java/com/prokofeva/calculator_api/doman/LoanOfferDto.java
@@ -2,26 +2,35 @@ package com.prokofeva.calculator_api.doman;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 public class LoanOfferDto {
 
-    private String statementId;
+    @NotNull
+    private UUID statementId;
 
+    @NotNull
     private BigDecimal requestedAmount;
 
+    @NotNull
     private BigDecimal totalAmount;
 
+    @NotNull
     private Integer term;
 
+    @NotNull
     private BigDecimal monthlyPayment;
 
+    @NotNull
     private BigDecimal rate;
 
+    @NotNull
     private Boolean isInsuranceEnabled;
 
+    @NotNull
     private Boolean isSalaryClient;
-
 }
 
--- calculator_api/src/main/java/com/prokofeva/calculator_api/service/CalculatorService.java
+++ calculator_api/src/main/java/com/prokofeva/calculator_api/service/CalculatorService.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CalculatorService {
+    private LoanOfferService loanOfferService;
     private Prescoring prescoring;
     private Scoring scoring;
 
@@ -21,21 +22,14 @@ public class CalculatorService {
         prescoring.isValid(loanStatementRequestDto);
 
         List<LoanOfferDto> offers = new ArrayList<>();
-        offers.add(calcOffer(loanStatementRequestDto, false, false));
-        offers.add(calcOffer(loanStatementRequestDto, false, true));
-        offers.add(calcOffer(loanStatementRequestDto, true, true));
-        offers.add(calcOffer(loanStatementRequestDto, true, false));
+        offers.add(loanOfferService.createLoanOffer(loanStatementRequestDto, false, false));
+        offers.add(loanOfferService.createLoanOffer(loanStatementRequestDto, false, true));
+        offers.add(loanOfferService.createLoanOffer(loanStatementRequestDto, true, true));
+        offers.add(loanOfferService.createLoanOffer(loanStatementRequestDto, true, false));
 
         offers.sort((of1, of2) -> of2.getRate().compareTo(of1.getRate()));
 
         return offers;
-    }
-
-    private LoanOfferDto calcOffer(LoanStatementRequestDto loanStatementRequestDto, boolean isInsuranceEnabled, boolean isSalaryClient) {
-        LoanOfferDto offer = new LoanOfferDto();
-
-
-        return offer;
     }
 
     public ResponseEntity<CreditDto> calculateLoan(ScoringDataDto scoringDataDto) {
