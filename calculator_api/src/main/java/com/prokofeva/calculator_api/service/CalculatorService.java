671d97ab4ff99fa36c42a5de08cc8ca3da5bdbad
MariyaPro <mariaprokof@gmail.com>
2024-05-23 11:25:26 +0300

    refactoring CalculatorService

--- calculator_api/src/main/java/com/prokofeva/calculator_api/service/CalculatorService.java
+++ calculator_api/src/main/java/com/prokofeva/calculator_api/service/CalculatorService.java
@@ -2,23 +2,26 @@ package com.prokofeva.calculator_api.service;
 
 import com.prokofeva.calculator_api.doman.*;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Setter
 public class CalculatorService {
 
     @Value("${base_loan_rate}")
     private Double baseRate;
+
+    private final MathContext mathContextDoc = new MathContext(2);
+    private final MathContext mathContextCalc = new MathContext(10);
 
     public List<LoanOfferDto> createListOffer(LoanStatementRequestDto loanStatementRequestDto) {
         if (!prescoring(loanStatementRequestDto)) throw new RuntimeException(); // todo bag request
@@ -41,17 +44,23 @@ public class CalculatorService {
 
     private LoanOfferDto createLoanOffer(LoanStatementRequestDto loanStatementRequestDto,
                                          boolean isInsuranceEnabled, boolean isSalaryClient) {
+        BigDecimal amount = loanStatementRequestDto.getAmount();
+        Integer term = loanStatementRequestDto.getTerm();
+
         BigDecimal rate = calculateRate(isInsuranceEnabled, isSalaryClient);
 
-        BigDecimal totalAmount = calculateTotalAmount(loanStatementRequestDto.getAmount(),
-                loanStatementRequestDto.getTerm(), rate, isInsuranceEnabled);
+        BigDecimal monthlyPayment = calculateMonthlyPayment(amount, term, rate);
+
+        BigDecimal insurance = isInsuranceEnabled ? calculateInsurance(amount, term) : BigDecimal.ZERO;
+
+        BigDecimal psk = calculatePsk(term, monthlyPayment, insurance);
 
         LoanOfferDto offerDto = new LoanOfferDto();
         offerDto.setStatementId(UUID.randomUUID());
-        offerDto.setRequestedAmount(loanStatementRequestDto.getAmount());
-        offerDto.setTotalAmount(totalAmount);
-        offerDto.setTerm(loanStatementRequestDto.getTerm());
-        offerDto.setMonthlyPayment(totalAmount.multiply(BigDecimal.valueOf(1.0 / loanStatementRequestDto.getTerm())));
+        offerDto.setRequestedAmount(amount);
+        offerDto.setTotalAmount(psk);
+        offerDto.setTerm(term);
+        offerDto.setMonthlyPayment(monthlyPayment);
         offerDto.setRate(rate);
         offerDto.setIsInsuranceEnabled(isInsuranceEnabled);
         offerDto.setIsSalaryClient(isSalaryClient);
@@ -74,7 +83,7 @@ public class CalculatorService {
         if (amount.compareTo(BigDecimal.valueOf(100000.0)) <= 0) rate += 1.0;
         if (amount.compareTo(BigDecimal.valueOf(1000000.0)) >= 0) rate -= 1.0;
 
-        BigDecimal amountInsurance = amount.multiply(BigDecimal.valueOf(rate/100.0)).multiply(BigDecimal.valueOf(term / 12.0));
+        BigDecimal amountInsurance = amount.multiply(BigDecimal.valueOf(rate * 0.01)).multiply(BigDecimal.valueOf(term / 12.0));
 
         BigDecimal maxInsurance = amount.multiply(BigDecimal.valueOf(0.1));
         BigDecimal minInsurance = amount.multiply(BigDecimal.valueOf(0.05));
@@ -82,31 +91,53 @@ public class CalculatorService {
         return amountInsurance.max(minInsurance).min(maxInsurance);
     }
 
-    private BigDecimal calculateTotalAmount(BigDecimal amount, Integer term, BigDecimal rate, boolean isInsuranceEnabled) {
-        BigDecimal totalAmount = isInsuranceEnabled ? amount.add(calculateInsurance(amount, term)) : amount;
-        totalAmount = totalAmount.add(totalAmount
-                .multiply(rate)
-                .multiply(BigDecimal.valueOf(term / 12.0)));
-        return totalAmount;
+    // monthlyPayment = amount * rate / 12 * (1+rate/12)^term / ((1+rate/12)^term -1)  ,
+    private BigDecimal calculateMonthlyPayment(BigDecimal amount, Integer term, BigDecimal rate) {
+        BigDecimal rateMonth = rate.divide(BigDecimal.valueOf(12), mathContextCalc);
+        BigDecimal helper = rateMonth.add(BigDecimal.ONE).pow(term);//todo name? helper=(1+rate/12)^term
+
+        BigDecimal monthlyPayment = amount.multiply(rateMonth).multiply(helper).divide(helper.add(BigDecimal.valueOf(-1)), mathContextDoc);
+
+        return monthlyPayment;
+    }
+
+    // вроде лишнее
+//    private BigDecimal calculateTotalAmount(BigDecimal amount, Integer term, BigDecimal monthlyPayment, boolean isInsuranceEnabled) {
+//        BigDecimal totalAmount = monthlyPayment.multiply(BigDecimal.valueOf(term));
+//        if (isInsuranceEnabled) totalAmount = totalAmount.add(calculateInsurance(amount, term));
+//        return totalAmount;
+//    }
+
+    private BigDecimal calculatePsk(Integer term, BigDecimal monthlyPayment, BigDecimal insurance) {
+        return monthlyPayment.multiply(BigDecimal.valueOf(term)).add(insurance);
     }
 
     public CreditDto calculateCredit(ScoringDataDto scoringDataDto) {
+        BigDecimal amount = scoringDataDto.getAmount();     //todo повтор кода
+        Integer term = scoringDataDto.getTerm();
+        BigDecimal insurance = scoringDataDto.getIsInsuranceEnabled() ? calculateInsurance(amount, term) : BigDecimal.ZERO;     //todo повтор кода
+
         BigDecimal totalRate = scoring(scoringDataDto);
 
-        List<PaymentScheduleElementDto>
+        BigDecimal monthlyPayment = calculateMonthlyPayment(amount, term, totalRate);
+
+        BigDecimal psk = calculatePsk(term, monthlyPayment, insurance);
+
+        List<PaymentScheduleElementDto> schedule = createPaymentSchedule(amount, term, totalRate, monthlyPayment);
 
         CreditDto creditDto = new CreditDto();
-        creditDto.setAmount(scoringDataDto.getAmount());
-        creditDto.setTerm(scoringDataDto.getTerm());
-        creditDto.setMonthlyPayment();
+        creditDto.setAmount(amount);
+        creditDto.setTerm(term);
+        creditDto.setMonthlyPayment(monthlyPayment);
         creditDto.setRate(totalRate);
-        creditDto.setPsk();
+        creditDto.setPsk(psk);
         creditDto.setIsInsuranceEnabled(scoringDataDto.getIsInsuranceEnabled());
         creditDto.setIsSalaryClient(scoringDataDto.getIsSalaryClient());
-        creditDto.setPaymentSchedule();
+        creditDto.setPaymentSchedule(schedule);
 
         return creditDto;
     }
+
 
     private BigDecimal scoring(ScoringDataDto scoringDataDto) {
         BigDecimal rate = calculateRate(scoringDataDto.getIsInsuranceEnabled()
@@ -115,7 +146,7 @@ public class CalculatorService {
         switch (scoringDataDto.getEmployment().getEmploymentStatus()) {
             case UNEMPLOYED -> throw new RuntimeException("отказ"); //todo
             case SELF_EMPLOYED -> rate = rate.add(BigDecimal.ONE);
-            case BUSINESS_OWNER -> rate = rate.add(BigDecimal.TWO);
+            case BUSINESS_OWNER -> rate = rate.add(BigDecimal.valueOf(2));
         }
 
         switch (scoringDataDto.getEmployment().getPosition()) {
@@ -155,5 +186,27 @@ public class CalculatorService {
             throw new RuntimeException("отказ"); // todo
 
         return rate;
+    }
+
+    private List<PaymentScheduleElementDto> createPaymentSchedule(BigDecimal amount, Integer term,
+                                                                  BigDecimal totalRate,
+                                                                  BigDecimal monthlyPayment
+    ) {
+        List<PaymentScheduleElementDto> schedule = new ArrayList<>();
+
+        BigDecimal remainingDebt = amount;
+        BigDecimal totalPayment = monthlyPayment;
+        BigDecimal rateDay = totalRate.multiply(BigDecimal.valueOf(365));
+        LocalDate date = LocalDate.now();
+        for (int i = 1; i < term; i++) {
+            LocalDate datePayment = date.plusMonths(1);
+            long days = ChronoUnit.DAYS.between(date, datePayment);
+            BigDecimal interestPayment = remainingDebt.multiply(rateDay).multiply(BigDecimal.valueOf(days)).round(mathContextDoc);
+// todo закончить
+
+
+        }
+
+        return schedule;
     }
 }
